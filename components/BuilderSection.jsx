bd4c99d48e8b62e2ef055969164b416196f49f7d
AAVRTI\deepak <deepak@aavrti.com>
2025-07-01 17:26:14 +0530

    Modified the navbar and added the logo

--- components/BuilderSection.jsx
+++ components/BuilderSection.jsx
@@ -61,7 +61,7 @@ export default function BuilderSection() {
               ))}
             </div>
             <div className="flex items-center gap-3 min-w-5xl">
-              <button className="flex items-center gap-1 group bg-gradient-to-tl from-[#377ef4] via-[#2563eb] to-[#3b82f6] hover:brightness-110 px-4 py-3 font-grotesk-medium font-medium text-sm rounded-lg shadow-lg cursor-pointer">
+              <button className="flex items-center gap-1 group bg-gradient-to-tl from-[#377ef4] via-[#2563eb] to-[#3b82f6] hover:brightness-110 px-4 py-3 font-grotesk-medium font-medium text-sm rounded-lg shadow-lg cursor-none">
                 Start Free Trial
                 <ArrowRight
                   className="group-hover:translate-x-2 transition-all duration-300"
@@ -69,7 +69,7 @@ export default function BuilderSection() {
                   color="#92c5fd"
                 />
               </button>
-              <button className="bg-white px-4 py-3 text-black font-grotesk-medium text-sm shadow-lg font-medium rounded-lg cursor-pointer hover:bg-gray-50">
+              <button className="bg-white px-4 py-3 text-black font-grotesk-medium text-sm shadow-lg font-medium rounded-lg cursor-none hover:bg-gray-50">
                 Learn More
               </button>
             </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,36 +2,37 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="min-w-[1100px] mx-auto mt-6 sticky z-10">
+    <nav className="w-full  mx-auto mt-6 sticky z-10 px-4">
       <div className="flex justify-between items-center p-3 rounded-2xl">
-        <Image
-          src="/images/logo.svg"
-          width={28}
-          height={28}
-          alt="simple-logo"
-        />
         <div className="flex items-center gap-12">
-          <a className="text-black text-sm font-inter font-normal" href="#">
+          <a className="text-white font-grotesk-light text-sm font-normal" href="#">
             Pricing
           </a>
-          <a className="text-black text-sm font-inter font-normal" href="#">
+          <a className="text-white font-grotesk-light text-sm font-normal" href="#">
             Customers
           </a>
-          <a className="text-black text-sm font-inter font-normal" href="#">
+          <a className="text-white font-grotesk-light text-sm font-normal" href="#">
             Blogs
           </a>
-          <a className="text-black text-sm font-inter font-normal" href="#">
+          <a className="text-white font-grotesk-light text-sm font-normal" href="#">
             Docs
           </a>
-          <a className="text-black text-sm font-inter font-normal" href="#">
+          <a className="text-white font-grotesk-light text-sm font-normal" href="#">
             Extra
           </a>
         </div>
+        <Image
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          src="/images/logo.png"
+          width={100}
+          height={100}
+          alt="nexlify-logo"
+        />
         <div className="flex items-center gap-3">
-          <button className="border-[.5px] border-gray-100 px-3 py-1.5 rounded-lg text-sm text-black font-inter cursor-pointer shadow-sm hover:bg-gray-50">
+          <button className="bg-white border-[.5px] border-gray-100 px-3 py-1.5 rounded-lg text-sm text-black font-grotesk-medium cursor-pointer shadow-sm hover:bg-gray-50">
             Login
           </button>
-          <button className="bg-black px-3 py-1.5 rounded-lg text-sm text-white font-inter cursor-pointer shadow-xs">
+          <button className="bg-white px-3 py-1.5 rounded-lg text-sm text-black font-grotesk-medium cursor-pointer shadow-xs">
             Register
           </button>
         </div>
